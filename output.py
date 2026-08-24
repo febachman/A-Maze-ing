79c1b9c8546aa2da37d29324dddbff27ea9baccf
Mateus Henrique De Lucas <made-luc@c2r2p2.42sp.org.br>
2026-08-24 15:01:44 -0300

    Add: adding initial outpuy config

---
+++ output.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    from maze_generator import MazeGenerator
+    from config_parser import read_config
+
+    config = read_config("config.txt")
+
+    generator = MazeGenerator(
+        width=config["WIDTH"],
+        height=config["HEIGHT"]
+    )   
+    print("\n##### A-MAZE-ING #####\n")
+    generator.display_debug()
