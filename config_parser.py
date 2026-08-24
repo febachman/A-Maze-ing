a1e9659546da5d73529c0d0e7f314a6d3b1b4cad
Mateus Henrique De Lucas <made-luc@c2r2p2.42sp.org.br>
2026-08-24 14:21:19 -0300

    Add: config parser and maze structure

---
+++ config_parser.py
@@ -0,0 +1,26 @@
+def read_config(filepath):
+    config = {}
+    with open(filepath, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            key, value = line.split("=")
+
+            if key == "WIDTH":
+                config[key] = int(value)
+            elif key == "HEIGHT":
+                config[key] = int(value)
+            elif key == "ENTRY" or key == "EXIT":
+                x, y = value.split(",")
+                config[key] = (int(x), int(y))
+            elif key == "PERFECT":
+                config[key] = (value == "True")
+            else:
+                config[key] = value
+    req_keys = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "PERFECT"]
+    for req in req_keys:
+        if req not in config:
+            print("Error: Missing key", req)
+            exit(1)
+    return config
