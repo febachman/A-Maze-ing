12a967d011022bc36edf91e41adbe3cf6c31327e
Mateus Henrique De Lucas <made-luc@c2r2p2.42sp.org.br>
2026-08-28 10:27:10 -0300

    feat: refactoring maze grid using box-drawing characters.

--- display.py
+++ display.py
@@ -101,7 +101,7 @@ def convert_to_box_drawing(
         grid: typing.List[typing.List[str]]
 ) -> typing.List[typing.List[str]]:
     BOX_CHARS_DICT = {
-        0: '■',  1: '│',  2: '─',  3: '└',
+        0: '*',  1: '│',  2: '─',  3: '└',
         4: '│',  5: '│',  6: '┌',  7: '├',
         8: '─',  9: '┘', 10: '─', 11: '┴',
         12: '┐', 13: '┤', 14: '┬', 15: '┼'
